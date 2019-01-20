723f4c5b8d9fe55e3178b37c1a839abb63069594
PositivDK <46874780+PositivDK@users.noreply.github.com>
2019-01-20 23:00:55 +0100

    Add files via upload

---
+++ Tiles/ExpTable.cs
@@ -0,0 +1,64 @@
+﻿using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+
+namespace GGD.Tiles
+{
+    public class ExpTable : ModTile
+    {
+        public override void SetDefaults()
+        {
+            /*Main.tileShine[Type] = 1100;
+            Main.tileSolid[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(200, 200, 200), Language.GetText("MapObject.MetalBar")); // localized text for "Metal Bar"*/
+            Main.tileSolidTop[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileTable[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 18 };
+            TileObjectData.addTile(Type);
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Exp Table");
+            AddMapEntry(new Color(200, 200, 200), name);
+            dustType = DustID.Confetti;
+            disableSmartCursor = true;
+            adjTiles = new int[] { TileID.WorkBenches };
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+
+        /*public override bool Drop(int i, int j)
+        {
+            Tile t = Main.tile[i, j];
+            int style = t.frameX / 18;
+            if (style == 0) // It can be useful to share a single tile with multiple styles. This code will let you drop the appropriate bar if you had multiple.
+            {
+                Item.NewItem(i * 16, j * 16, 16, 16, mod.ItemType("ExampleBar"));
+            }
+            return base.Drop(i, j);
+            
+        }*/
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 32, 32, mod.ItemType("ExpTableItem"));
+        }
+    }
+}
